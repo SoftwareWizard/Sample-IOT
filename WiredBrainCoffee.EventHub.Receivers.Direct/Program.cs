16e0c80427ad705d8f3e81759f8473c3e36d582a
Simon Bauer <simon.bauer@software-wizard.de>
2018-03-02 16:59:38 +0100

    Receiver implemented

---
+++ WiredBrainCoffee.EventHub.Receivers.Direct/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+
+namespace WiredBrainCoffee.EventHub.Receivers.Direct
+{
+    public static class Program
+    {
+        private static EventHubClient _eventHubClient;
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                _eventHubClient = CreateEventHubClient();
+                await ReceiveData();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            Console.WriteLine("-- Press Key --");
+            Console.ReadKey();
+            _eventHubClient.Close();
+        }
+
+        private static async Task ReceiveData()
+        {
+            var runTimeInformation = await _eventHubClient.GetRuntimeInformationAsync();
+
+            var partitionReceivers = runTimeInformation
+                .PartitionIds
+                .Select(id => _eventHubClient.CreateReceiver("$Default", id, EventPosition.FromStart()));
+
+            Console.WriteLine("Waiting for incoming Data");
+            foreach (var partitionReceiver in partitionReceivers)
+            {
+                partitionReceiver.SetReceiveHandler(new WiredBrainCoffeePartitionReceiveHandler(partitionReceiver.PartitionId));
+            }
+        }
+
+        private static EventHubClient CreateEventHubClient()
+        {
+            Console.WriteLine("Connecting to EventHub");
+            var eventHubConnectionString = ConfigurationManager.ConnectionStrings["eventHub"].ConnectionString;
+            var eventHubClient = EventHubClient.CreateFromConnectionString(eventHubConnectionString);
+            return eventHubClient;
+        }
+    }
+}
---
+++ WiredBrainCoffee.EventHub.Receivers.Direct/WiredBrainCoffeePartitionReceiveHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+
+namespace WiredBrainCoffee.EventHub.Receivers.Direct
+{
+    public class WiredBrainCoffeePartitionReceiveHandler : IPartitionReceiveHandler
+    {
+        public WiredBrainCoffeePartitionReceiveHandler(string partitionId)
+        {
+            PartitionId = partitionId;
+        }
+
+        public string PartitionId { get; }
+        public int MaxBatchSize { get; set; }
+
+        public Task ProcessEventsAsync(IEnumerable<EventData> events)
+        {
+            if (events != null)
+            { 
+                foreach (var eventData in events)
+                {
+                    var dataAsJson = Encoding.UTF8.GetString(eventData.Body.Array);
+                    Console.WriteLine($"{dataAsJson} | PartionId: {PartitionId}");
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessErrorAsync(Exception error)
+        {
+            Console.WriteLine($"Exception: {error.Message}");
+            return Task.CompletedTask;
+        }
+    }
+}
