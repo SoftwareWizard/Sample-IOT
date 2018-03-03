b80d6c07f3dcb39c184283626b0e1822cd1462bc
Simon Bauer <simon.bauer@software-wizard.de>
2018-03-03 18:33:02 +0100

    Added AzureFunction

---
+++ WiredBrainCoffee.FunctionApp/AlertFunction.cs
@@ -0,0 +1,26 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Newtonsoft.Json;
+using WiredBrainCoffee.EventHub.Model;
+
+namespace WiredBrainCoffee.FunctionApp
+{
+    public static class AlertFunction
+    {
+        [FunctionName("AlertFunction")]
+        public static void Run(
+            [EventHubTrigger("wiredbraincoffeeeh",
+                Connection = "InputEventHubConnectionString",
+                ConsumerGroup = "wired_brain_coffee_alert_function")]
+            string[] inputMessages,
+            TraceWriter log)
+        {
+            foreach (var inputMessage in inputMessages)
+            {
+                var data = JsonConvert.DeserializeObject<CoffeeMachineData>(inputMessage);
+                log.Info($"{data}");
+            }
+        }
+    }
+}
